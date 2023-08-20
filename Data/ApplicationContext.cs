69e3246589c46a663a4f1953167c3aba437a34e2
Şevval Şenel <57571561+ssevvalsenel@users.noreply.github.com>
2023-08-20 13:24:40 +0300

    Add project files.

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using bookDemo.Models;
+
+namespace bookDemo.Data
+{
+    public static class ApplicationContext
+    {
+        public static List<Book> Books { get; set; }
+        static ApplicationContext()
+        {
+            Books = new List<Book>()
+            {
+                new Book() {Id=1, Title="Kitap1", Price=75},
+                new Book() {Id=2, Title="Kitap2", Price=100},
+                new Book() {Id=3, Title="Kitap3", Price=125}
+            };
+        }
+    }
+}
